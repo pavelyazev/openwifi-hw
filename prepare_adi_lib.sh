bf8eb6f71c10f9a65054bf314352aae7344ceed2
Pavel Yazev <pyazev@gmail.com>
2024-12-23 16:08:09 +0100

    adi_lib build script modified with an available Vivado version

--- prepare_adi_lib.sh
+++ prepare_adi_lib.sh
@@ -25,12 +25,12 @@ cd ./adi-hdl/
 # git reset --hard f61d9707eb0a62533efd6facab59ab2444da94c9
 git reset --hard
 git fetch
-git checkout 2021_r1
-git reset --hard 2021_r1
+git checkout hdl_2021_r2
+#git reset --hard 2021_r2
 
 # # the lib need to be built!
-source $XILINX_DIR/Vivado/2021.1/settings64.sh
+source $XILINX_DIR/Vivado/2023.2/settings64.sh
 cd library/
-make
+make ADI_IGNORE_VERSION_CHECK=1
 
 cd $home_dir
